357364dcf38885c6b619401d53246e9daaadf36c
Michael Ford <fanquake@users.noreply.github.com>
2015-12-18 16:15:27 +0800

    Secp256k1 c18b869e58aa4d3bff6958f370f6b643d1223c44

--- Formula/secp256k1.rb
+++ Formula/secp256k1.rb
@@ -1,7 +1,7 @@
 class Secp256k1  < Formula
   desc "Optimized C library for EC operations on curve secp256k1"
   homepage "https://github.com/bitcoin/secp256k1"
-  head "https://github.com/bitcoin/secp256k1.git", :revision => "3026daa095797ffb5371ecf19bbd1de3d3b3eeb7"
+  head "https://github.com/bitcoin/secp256k1.git", :revision => "c18b869e58aa4d3bff6958f370f6b643d1223c44"
 
   depends_on "automake" => :build
   depends_on "autoconf" => :build
